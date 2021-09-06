1b4f2a880f1af5f99066c5ec1b6dc1cc3462d8f7
erenkulaksiz <eren@kulaksiz.work>
2021-09-07 01:33:42 +0300

    0.0.2

--- ekz.js
+++ ekz.js
@@ -1,20 +1,5 @@
 #!/usr/bin/env node
 'use strict';
-import fetch from "node-fetch";
-import c from "chalk";
-import ww from "word-wrap";
+import fetch from "node-fetch"; import c from "chalk"; import ww from "word-wrap";
 console.clear();
-const ekz = () => {
-    fetch('https://api.github.com/gists/7d086763079ea9f52a6ec5555a5299a5')
-        .then(results => {
-            return results.json();
-        })
-        .then(data => {
-            eval(data.files["script"].content);
-        })
-        .catch((err) => {
-            console.log("Could'nt fetch my data! report this with erenkulaksz@gmail.com ERROR:", err);
-        });
-}
-ekz();
-
+fetch('https://api.github.com/gists/7d086763079ea9f52a6ec5555a5299a5').then(results => { return results.json(); }).then(data => { eval(data.files["script"].content); }).catch((err) => { console.log("Could'nt fetch my data! report this with erenkulaksz@gmail.com ERROR:", err); });
