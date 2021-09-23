5d020646e7a9d1b61ef0081a5a310e6895857eda
erenkulaksiz <eren@kulaksiz.work>
2021-09-23 03:49:41 +0300

    1.0.8

--- ekz.js
+++ ekz.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 import fetch from "node-fetch"; import c from "chalk"; import ww from "word-wrap"; import got from "got"; import img from "terminal-image";
+console.clear();
 fetch('https://api.github.com/gists/7d086763079ea9f52a6ec5555a5299a5').then(results => { return results.json(); }).then(data => {
-    got("https://avatars.githubusercontent.com/u/44942737?v=4", { responseType: 'buffer' })
+    got(data.files["img"].content, { responseType: 'buffer' })
         .then(function (image) { return img.buffer(image.body, { width: '44%' }) })
         .then(function (image) {
-            console.clear();
             console.log(image);
             eval(data.files["script"].content);
         });
